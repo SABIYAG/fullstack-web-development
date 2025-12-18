fb2fd81973a20373d94bfe94792a7d011e9256e8
SABIYAG <sabiyagobi2005@gmail.com>
2025-12-18 11:02:41 +0530

    Create LoginFormValidation App.jsx

---
+++ LoginFormValidation App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginFormValidation from "./FullStack/Sample/LoginFormValidation";
+
+function App() {
+  return <LoginFormValidation />;
+}
+
+export default App;
